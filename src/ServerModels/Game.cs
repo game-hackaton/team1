b54fb7354f8690c9850cba444b7946f28a738513
Stas <kretatusha@gmail.com>
2023-07-02 15:26:56 +0500

    dev move

--- src/ServerModels/Game.cs
+++ src/ServerModels/Game.cs
@@ -60,7 +60,7 @@ public class Game
         }
         AddNewCell();
     }
-
+    
     public CellDto[] PaintGame()
     {
         var testCells = new List<CellDto>();
@@ -70,5 +70,110 @@ public class Game
         }
         return testCells.ToArray();
     }
-    
+
+    public void Move(Direction dir)
+    {
+        int begin = -1;
+        int end = -1;
+        int napr = -1;
+        switch (dir)
+        {
+            case Direction.LEFT:
+                begin = -1;
+                end = Width;
+                napr = 1;
+                break;
+            case Direction.RIGHT:
+                begin = Width;
+                end = -1;
+                napr = -1;
+                break;
+            case Direction.UP:
+                begin = Height;
+                end = -1;
+                napr = -1;
+                break;
+            case Direction.DOWN:
+                begin = -1;
+                end = Height;
+                napr = 1;
+                break;
+        }
+
+        if (dir == Direction.UP || dir == Direction.DOWN)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                int[] oldRow = new int[Height];
+                for (int j = 0; j < Height; j++)
+                {
+                    oldRow[j] = Cells.First((c => c.pos == new Vector {X = i, Y = j})).content;
+                }
+
+                int[] newRow = MoveRow(oldRow, begin, end, napr);
+                for (int j = 0; j < Height; j++)
+                {
+                    Cells.First((c => c.pos == new Vector {X = i, Y = j})).content = newRow[j];
+                }
+            }
+        }
+        else
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                int[] oldRow = new int[Width];
+                for (int i = 0; i < Width; i++)
+                {
+                    oldRow[i] = Cells.First((c => c.pos == new Vector {X = i, Y = j})).content;
+                }
+
+                int[] newRow = MoveRow(oldRow, begin, end, napr);
+                for (int i = 0; i < Width; i++)
+                {
+                    Cells.First((c => c.pos == new Vector {X = i, Y = j})).content = newRow[i];
+                }
+            }
+        }
+    }
+
+    private int[] MoveRow(int[] oldRow, int begin, int end, int napr)
+    {
+        int[] oldRowWithoutZeroes = new int[oldRow.Length];
+        int q = begin;
+        int i = begin;
+        while ((napr == 1 && i<end)||(napr == -1 && i>end))
+        {
+            if (oldRow[i] != 0)
+            {
+                oldRowWithoutZeroes[q] = oldRow[i];
+                q+=napr;
+            }
+
+            i += napr;
+        }
+
+        int[] shiftedRow = new int[oldRowWithoutZeroes.Length];
+        q = begin;
+        int index = begin;
+        while ((napr == 1 && index < oldRowWithoutZeroes.Length) || (napr == -1 && index > 0))
+
+        {
+            if ((index + napr < oldRowWithoutZeroes.Length) && (oldRowWithoutZeroes[index] ==
+                                                                oldRowWithoutZeroes[index + napr])
+                                                            && oldRowWithoutZeroes[index] != 0)
+            {
+                shiftedRow[q] = oldRowWithoutZeroes[index] * 2;
+                index++;
+            }
+            else
+            {
+                shiftedRow[q] = oldRowWithoutZeroes[index];
+            }
+
+            q += napr;
+            index += napr;
+        }
+
+        return shiftedRow;
+    }
 }
