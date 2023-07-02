64f7fe817306749ef44028cd72d7a30c9d8f0d48
Vladislav <kvlad200@mail.ru>
2023-07-02 14:45:11 +0500

    Add method for adding new cell in start round

--- src/ServerModels/Cell.cs
+++ src/ServerModels/Cell.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.Numerics;
 using thegame.Models;
 
@@ -9,15 +10,29 @@ public class Cell
     private static int IdCount = 0;
     public Vector pos { get; set; }
     public int id { get; }
-    public string type { get; set; }
+
+    private static Dictionary<int, string> type = new Dictionary<int, string>
+    {
+        { 0, "td" },
+        { 2, "tile-2" },
+        { 4, "tile-4" },
+        {8, "tile-8"},
+        {16, "tile-16"},
+        {32, "tile-32"},
+        {64, "tile-64"},
+        {128, "tile-128"},
+        {256, "tile-256"},
+        {512, "tile-512"},
+        {1024, "tile-1024"},
+        {2048, "tile-2048"},
+    };
     public int content { get; set; }
     public int zindex { get; set; }
     
-    public Cell(Vector pos, string type, int content)
+    public Cell(Vector pos, int content)
     {
         id = ++IdCount;
         this.pos = pos;
-        this.type = type;
         this.content = content;
         zindex = 0;
     }
@@ -25,7 +40,7 @@ public class Cell
     public CellDto ToDto()
     {
         var res = (content == 0) ? "" : content.ToString();
-        return new CellDto(id.ToString(), pos.ToDto(), type, res, zindex);
+        return new CellDto(id.ToString(), pos.ToDto(), type[content], res, zindex);
     }
     
 }
--- src/ServerModels/Game.cs
+++ src/ServerModels/Game.cs
@@ -11,6 +11,12 @@ public class Game
     public int Width { get; set; }
     public int Height { get; set; }
 
+    private bool Finish;
+    public bool IsFinish
+    {
+        get { return Finish; }
+    }
+
     public Game(int Width, int Height)
     {
         Cells = new List<Cell>();
@@ -18,18 +24,42 @@ public class Game
         this.Height = Height;
     }
 
+    private void AddNewCell()
+    {
+        var emptyCell = new List<Cell>();
+        foreach (var cell in Cells)
+        {
+            if (cell.content == 0)
+            {
+                emptyCell.Add(cell);
+            }
+        }
+
+        if (emptyCell.Count == 0)
+        {
+            Finish = true;
+            return;
+        }
+
+        var rnd = new Random();
+        var indx = rnd.Next(0, emptyCell.Count);
+        emptyCell[indx].content = rnd.Next(1, 2) * 2;
+    }
+    
     public void ResetGame()
     {
         Cells.Clear();
+        Finish = false;
         for (int X = 0; X < Width; X++)
         {
             for (int Y = 0; Y < Height; Y++)
             {
-                Cells.Add(new Cell(new Vector{X = X, Y = Y}, "td", 0 ));
+                Cells.Add(new Cell(new Vector{X = X, Y = Y}, 0 ));
             }
         }
+        AddNewCell();
     }
-    
+
     public CellDto[] PaintGame()
     {
         var testCells = new List<CellDto>();
