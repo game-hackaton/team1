103569c0c0b9b6e60c995cc9035d5c53c90b2303
Alexandr Malyutin <zhiecie999@gmail.com>
2023-07-02 17:26:05 +0700

    add changes in MoveController

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -13,35 +13,9 @@ public class MovesController : Controller
     [HttpPost]
     public IActionResult Moves(Guid gameId, [FromBody] UserInputDto userInput)
     {
-        var d = (userInput.ClickedPos) ?? (new VectorDto { X = 1, Y = 1 });
-        var game = TestData.AGameDto(d);
-        // var game = new GameDto(4, 4);
-        var cell = game.Cells.First(c => c.Type == "color4");
-        var dir = GetDirection(userInput.KeyPressed);
-        switch (dir)
-        {
-            case Direction.LEFT:
-            {
-                cell.Pos.X -= 1; 
-                break;
-            } 
-            case Direction.RIGHT:
-            {
-                cell.Pos.X += 1; 
-                break;
-            } 
-            case Direction.UP:
-            {
-                cell.Pos.Y += 1; 
-                break;
-            } 
-            case Direction.DOWN:
-            {
-                cell.Pos.Y -= 1; 
-                break;
-            } 
-        }
-        return Ok(game);
+        var game = GamesRepository.MakeChanges(gameId, GetDirection(userInput.KeyPressed));
+        var res = new GameDto(game.PaintGame(), true, true, game.Width, game.Height, game.id, game.IsFinish, 0);
+        return Ok(res);
     }
 
     private Direction GetDirection(int keyPressed)
