e1bcad9ab4e09960c1380f769a4d2da8430e5949
Vladislav <kvlad200@mail.ru>
2023-07-02 14:32:33 +0500

    cell fix

--- src/ServerModels/Game.cs
+++ src/ServerModels/Game.cs
@@ -7,28 +7,25 @@ namespace thegame.ServerModels;
 
 public class Game
 {
-    public List<Cell> FillCells { get; set; }
-    public List<Cell> EmptyCells { get; set; }
+    public List<Cell> Cells { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
 
     public Game(int Width, int Height)
     {
-        FillCells = new List<Cell>();
-        EmptyCells = new List<Cell>();
+        Cells = new List<Cell>();
         this.Width = Width;
         this.Height = Height;
     }
 
     public void ResetGame()
     {
-        FillCells.Clear();
-        EmptyCells.Clear();
+        Cells.Clear();
         for (int X = 0; X < Width; X++)
         {
             for (int Y = 0; Y < Height; Y++)
             {
-                EmptyCells.Add(new Cell(new Vector{X = X, Y = Y}, "td", 0 ));
+                Cells.Add(new Cell(new Vector{X = X, Y = Y}, "td", 0 ));
             }
         }
     }
@@ -36,11 +33,7 @@ public class Game
     public CellDto[] PaintGame()
     {
         var testCells = new List<CellDto>();
-        foreach (var cell in FillCells)
-        {
-            testCells.Add(cell.ToDto());
-        }
-        foreach (var cell in EmptyCells)
+        foreach (var cell in Cells)
         {
             testCells.Add(cell.ToDto());
         }
