ae9f6818577d09ae2e49030cdb0698ceb5b43422
krutoi <shimyshimya@mail.ru>
2024-03-30 13:16:58 +0500

    cell refactor

--- src/GameModels/Cell.cs
+++ src/GameModels/Cell.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Drawing;
+using System.Numerics;
 
 namespace thegame.GameModels;
 
 public class Cell
 {
-    public string Id { get; set; }
-    public Point Pos { get; set; }
-    public int ZIndex { get; set; }
+    public Cell(Vector2 position)
+    {
+        Pos = position;
+        Id = new Guid();
+    }
+    
+    public Guid Id { get; set; }
+    public Vector2 Pos { get; set; }
     public int Score { get; set; }
-    public string Content { get; set; }
 }
