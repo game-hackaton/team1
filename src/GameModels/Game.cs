bdc046ae8cc8feb20d515b8eba29b6735312923e
Arman Mukanov <mukanovarman777@gmail.com>
2024-03-30 13:49:18 +0500

    release Move

--- src/GameModels/Game.cs
+++ src/GameModels/Game.cs
@@ -56,7 +56,27 @@ public class Game
 
     public void Move(Direction direction)
     {
-        // if direction == left
+        switch (direction)
+        {
+            case Direction.Up:
+                MoveUp();
+                break;            
+            case Direction.Left:
+                MoveLeft();
+                break;
+            case Direction.Right:
+                MoveRight();
+                break;
+            case Direction.Down:
+                MoveDown();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+    }
+
+    private void MoveLeft()
+    {
         for (var i = 0; i < Width; i++)
         {
             for (var j = 0; j < Height; j++)
@@ -64,7 +84,67 @@ public class Game
                 if (i == 0 || i == Width - 1) continue;
                 if (j == 0 || j == Height - 1) continue;
                 var current = Cells[i, j];
-                var previous = Cells[i - 1, j - 1];
+                var previous = Cells[i - 1, j];
+                if (current.Score == previous.Score)
+                {
+                    previous.Score *= 2;
+                }
+
+                Cells[i, j] = null;
+            }
+        }
+    }
+    
+    private void MoveRight()
+    {
+        for (var i = Width - 1; i >= 0; i++)
+        {
+            for (var j = 0; j < Height; j++)
+            {
+                if (i == 0 || i == Width - 1) continue;
+                if (j == 0 || j == Height - 1) continue;
+                var current = Cells[i, j];
+                var previous = Cells[i + 1, j];
+                if (current.Score == previous.Score)
+                {
+                    previous.Score *= 2;
+                }
+
+                Cells[i, j] = null;
+            }
+        }
+    }
+    
+    private void MoveUp()
+    {
+        for (var i = 0; i < Width; i++)
+        {
+            for (var j = 0; j < Height; j++)
+            {
+                if (i == 0 || i == Width - 1) continue;
+                if (j == 0 || j == Height - 1) continue;
+                var current = Cells[i, j];
+                var previous = Cells[i, j - 1];
+                if (current.Score == previous.Score)
+                {
+                    previous.Score *= 2;
+                }
+
+                Cells[i, j] = null;
+            }
+        }
+    }
+    
+    private void MoveDown()
+    {
+        for (var i = 0; i < Width; i++)
+        {
+            for (var j = Height - 1; j >= 0; j++)
+            {
+                if (i == 0 || i == Width - 1) continue;
+                if (j == 0 || j == Height - 1) continue;
+                var current = Cells[i, j];
+                var previous = Cells[i, j + 1];
                 if (current.Score == previous.Score)
                 {
                     previous.Score *= 2;
