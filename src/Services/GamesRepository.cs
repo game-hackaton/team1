f790a81064a8f1f4bc62b7830f52a4af34e852df
Arman Mukanov <mukanovarman777@gmail.com>
2024-03-30 16:10:44 +0500

    create repository

--- src/Services/GamesRepository.cs
+++ src/Services/GamesRepository.cs
@@ -1,5 +1,21 @@
+using System;
+using System.Collections.Generic;
+using thegame.GameModels;
+
 namespace thegame.Services;
 
 public class GamesRepository
 {
+    private Dictionary<Guid, Game> dictionary = new();
+
+    public void Add(Game game)
+    {
+        dictionary[game.Id] == game;
+    }
+
+    public Game? FindById(Guid id)
+    {
+        dictionary.TryGetValue(id, out var game);
+        return game;
+    }
 }
