9fcaf003ea6456856ac21f65942aff57d0a8dce3
Yrwlcm <free.gord@yandex.ru>
2024-03-30 13:44:24 +0300

    Конвертация интов в дирекшон и наоборот

--- src/Extensions/DirectionExtensions.cs
+++ src/Extensions/DirectionExtensions.cs
@@ -10,4 +10,16 @@ public static class DirectionExtensions
     {
         throw new NotImplementedException();
     }
+
+    public static int GetKeycode(this Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Left => 37,
+            Direction.Up => 38,
+            Direction.Right => 39,
+            Direction.Down => 40,
+            _ => 0
+        };
+    }
 }
